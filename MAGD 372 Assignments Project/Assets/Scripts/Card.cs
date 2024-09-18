d7ae6239a68f0eccbb86bf03686019ce80588484
Alloyed-Code <118241234+Alloyed-Code@users.noreply.github.com>
2024-09-17 23:29:41 -0500

    Repaired A3 and added Scriptable Objects

---
+++ MAGD 372 Assignments Project/Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Blank Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+
+    public string description;
+    public int cost;
+
+    public Sprite artwork;
+
+}
