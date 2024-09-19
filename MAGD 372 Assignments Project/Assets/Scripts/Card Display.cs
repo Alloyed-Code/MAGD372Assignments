fa24bb8663a339ba868c2c99889c4e67fa523d28
Alloyed-Code <118241234+Alloyed-Code@users.noreply.github.com>
2024-09-18 20:18:46 -0500

    finished scriptable objects assignment

--- MAGD 372 Assignments Project/Assets/Scripts/Card Display.cs
+++ MAGD 372 Assignments Project/Assets/Scripts/Card Display.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
+    public Card card;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+
+    public TextMeshProUGUI costText;
+
+    public Image artworkImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+        costText.text = card.cost.ToString();
+        artworkImage.sprite = card.artwork;
     }
 }
